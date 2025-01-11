17ba45a5ef8b62a596a45a4650e0693e93bd2beb
Andy Pernsteiner <andypern@gmail.com>
2025-01-10 20:05:14 -0800

    feat: add -t flag to launch.sh to specify docker image tag

--- launch.sh
+++ launch.sh
@@ -4,6 +4,22 @@
 CONTAINER_NAME="sauna-master"
 REPO=andypern
 
+# Default tag
+TAG="latest"
+
+# Parse command line arguments
+while getopts "t:" opt; do
+    case $opt in
+        t)
+            TAG="$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
 # Check if the container is running
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Container '$CONTAINER_NAME' is running. Stopping it..."
@@ -19,5 +35,5 @@ if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
 fi
 
 # Start the container again
-echo "Starting container '$CONTAINER_NAME'..."
-docker run --privileged --restart always --name $CONTAINER_NAME --device /dev/gpiomem --device /sys/bus/w1/devices:/sys/bus/w1/devices -p 8501:8501 -p 8080:8080 -d $REPO/$CONTAINER_NAME
+echo "Starting container '$CONTAINER_NAME' with tag '$TAG'..."
+docker run --privileged --restart always --name $CONTAINER_NAME --device /dev/gpiomem --device /sys/bus/w1/devices:/sys/bus/w1/devices -p 8501:8501 -p 8080:8080 -d $REPO/$CONTAINER_NAME:$TAG
